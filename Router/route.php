2a9cadd1c771af51c36d6719abd94b1a7ab7d7ad
Sreyvang <sreyvang.phon@student.passerellesnumeriques.org>
2025-02-26 20:29:32 +0700

    add invantory route

--- Router/route.php
+++ Router/route.php
@@ -3,9 +3,14 @@ require_once "Router.php";
 require_once "Controllers/BaseController.php";
 require_once "Database/Database.php";
 require_once "Controllers/DashboardController.php";
+require_once "Controllers/InventoryController.php";
 
 
 $route = new Router();
 $route->get("/", [DashboardController::class, 'dashboard']);
+
+
+// Inventory
+$route->get("/inventory", [InventoryController::class, 'index']);
 
 $route->route();
