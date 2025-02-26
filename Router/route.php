50d0508407855f9e2c1246272dac877fb116a73f
Sreyvang <sreyvang.phon@student.passerellesnumeriques.org>
2025-02-26 20:43:05 +0700

    add view sale page

--- Router/route.php
+++ Router/route.php
@@ -4,6 +4,7 @@ require_once "Controllers/BaseController.php";
 require_once "Database/Database.php";
 require_once "Controllers/DashboardController.php";
 require_once "Controllers/InventoryController.php";
+require_once "Controllers/SaleController.php";
 
 
 $route = new Router();
@@ -12,5 +13,9 @@ $route->get("/", [DashboardController::class, 'dashboard']);
 
 // Inventory
 $route->get("/inventory", [InventoryController::class, 'index']);
+
+
+// Sales
+$route->get("/sale", [SaleController::class, 'index']);
 
 $route->route();
