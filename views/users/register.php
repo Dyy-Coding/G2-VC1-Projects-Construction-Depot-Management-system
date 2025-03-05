8122a9e6d72b24415770ac9d341832d49a5efad2
Sreyvang <sreyvang.phon@student.passerellesnumeriques.org>
2025-03-05 19:30:13 +0700

    CV1-13 add btn cf-password

--- views/users/register.php
+++ views/users/register.php
@@ -2,14 +2,6 @@
   <main class="main-content  mt-0">
     <div class="page-header align-items-start min-vh-50 pt-5 pb-11 m-3 border-radius-lg" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signup-cover.jpg'); background-position: top;">
       <span class="mask bg-gradient-dark opacity-6"></span>
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col-lg-5 text-center mx-auto">
-            <h1 class="text-white mb-2 mt-5">Welcome!</h1>
-            <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p>
-          </div>
-        </div>
-      </div>
     </div>
     <div class="container">
       <div class="row mt-lg-n10 mt-md-n11 mt-n10 justify-content-center">
@@ -73,6 +65,9 @@
                 <div class="mb-3">
                   <input type="password" class="form-control" placeholder="Password" aria-label="Password">
                 </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control" placeholder="Confirm Password" aria-label="Confirm Password">
+                </div>
                 <div class="form-check form-check-info text-start">
                   <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
                   <label class="form-check-label" for="flexCheckDefault">
@@ -80,7 +75,7 @@
                   </label>
                 </div>
                 <div class="text-center">
-                  <button type="button" class="btn bg-gradient-dark w-100 my-4 mb-2">Sign up</button>
+                  <button type="button" class="btn bg-gradient-dark w-100 my-4 mb-2">Register</button>
                 </div>
                 <p class="text-sm mt-3 mb-0">Already have an account? <a href="javascript:;" class="text-dark font-weight-bolder">Sign in</a></p>
               </form>
